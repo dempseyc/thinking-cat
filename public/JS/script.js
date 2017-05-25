b0cf2a95ddeb4a958b16cdb25c0137ee1c487543
Craig Dempsey <craigdempsey@gmail.com>
2017-05-25 15:56:50 -0400

    build out jquery a bit more

--- public/JS/script.js
+++ public/JS/script.js
@@ -8,75 +8,68 @@ $( document ).ready(function() {
 
     let catStory = "";
 
-    let catData = "";
+    let catData = {};
 
     let rooms = [
       {
       "id": 0,
       "name": "title",
       "image": "/images/0-title.gif",
-      "status": "click to start"
+      "status": "click to start",
+      "options": ["click"]
       },
       {
       "id": 1,
       "name": "basement",
       "image": "/images/1-basement.gif",
-      "status": "hunting"
+      "status": "hunting",
+      "options": ["ladder","hole"]
       },
       {
       "id": 2,
       "name": "utility",
       "image": "/images/2-boiler.gif",
+      "options": ["back"],
       "status": "hunting"
       },
       {
       "id": 3,
       "name": "storage",
       "image": "/images/3-storage.gif",
+      "options": ["back","door"],
       "status": "hunting"
       },
       {
       "id": 4,
       "name": "hallway",
       "image": "/images/4-hallway.gif",
+      "options": ["door1","door2","window"],
       "status": "thinking"
       },
       {
       "id": 5,
       "name": "kitchen",
       "image": "/images/5-kitchen.gif",
+      "options": ["back","door22"],
       "status": "eating"
       },
       {
       "id": 6,
       "name": "outside",
       "image": "/images/6-outside.gif",
+      "options": [],
       "status": "YOU LOSE!"
       },
       {
       "id": 7,
       "name": "bedroom",
       "image": "/images/7-bedroom.gif",
+      "options": [],
       "status": "YOU WIN!"
       }
     ]
 
     let currentRoom = 1;
-    // build this with jquery
-
-    /*
-    <div class="message">
-    <p>YOU ARE IN THE BASEMENT</p>
-    </div>
-    <div class="image">
-    <h1>[]</h1>
-    </div>
-    <div class="options">
-    options
-    <h2>{}{}{}</h2>
-    </div>
-    <div class="status"><h3>"playing"</h3></div>
-    */
 
     let GameUnit = $('#game-unit');
 
@@ -88,27 +81,33 @@ $( document ).ready(function() {
     let IMG = $('<img>'); //works
     IMG.attr({src:imageSource});  //add altlater //works
 
-
-
     //build out buttons and append to OPT
     //
     let OPT = $('<form class="options">');
-    ////need to plan out the options form with multiple buttons, scaling depending on the number of buttons and options according to game-play
+    let BUTTS = [];
+
+    //swich case scenario, depending upon the currentRoom #;
+
+
+    //stuff like
+
+    let optionsArr = `${rooms[currentRoom]["options"]}`
+
+
+    ////need to plan out the options form with multiple buttons appended, scaling depending on the number of buttons and options according to game-play scenario
+
     let status = `${rooms[currentRoom]["status"]}`;
     let STAT = $('<div class="'+status+'">');
     STAT.text(status);
 
+    //DOM is painted
     GameUnit.append(M);
     GameUnit.append(IMG);
     GameUnit.append(OPT);
     GameUnit.append(STAT);
 
-    //still need to addchild to dom and append to GameUnit.
-    //then start debugging
-
-
-    //some functions for updating the currentRoom, game-unit, mouseCount, catStory, and catData
-    //functions for posting catStory and catData to db. i.e. buttons hit post routes on server
+    //some functions for updating the currentRoom, game-unit remove and append, mouseCount, catStory, and catData
+    //functions for posting catStory and catData obj to db. i.e. buttons hit post routes on server
 
     // M V P
 
