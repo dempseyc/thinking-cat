1eb720e5cac5d18395d7925c39e2e4b803ff6d64
Craig Dempsey <craigdempsey@gmail.com>
2017-05-25 14:45:28 -0400

    add jquery and es6 stuff, but not debugged

--- public/JS/script.js
+++ public/JS/script.js
@@ -11,43 +11,56 @@ $( document ).ready(function() {
 
     let rooms = [
       {
+      "id": 0,
+      "name": "title",
+      "image": "/images/0-title.gif",
+      "status": "click to start"
+      },
+      {
+      "id": 1,
       "name": "basement",
       "image": "/images/1-basement.gif",
       "status": "hunting"
       },
       {
+      "id": 2,
       "name": "utility",
       "image": "/images/2-boiler.gif",
       "status": "hunting"
       },
       {
+      "id": 3,
       "name": "storage",
       "image": "/images/3-storage.gif",
       "status": "hunting"
       },
       {
+      "id": 4,
       "name": "hallway",
       "image": "/images/4-hallway.gif",
       "status": "thinking"
       },
       {
+      "id": 5,
       "name": "kitchen",
       "image": "/images/5-kitchen.gif",
       "status": "eating"
       },
       {
+      "id": 6,
       "name": "outside",
       "image": "/images/6-outside.gif",
       "status": "YOU LOSE!"
       },
       {
+      "id": 7,
       "name": "bedroom",
       "image": "/images/7-bedroom.gif"
       "status": "YOU WIN!"
       }
     ]
 
-    let GameUnit = $('#game-unit');
+    let currentRoom = 1;
     // build this with jquery
 
     /*
@@ -61,10 +74,28 @@ $( document ).ready(function() {
     options
     <h2>{}{}{}</h2>
     </div>
-    <div class="message"><h3>"playing"</h3></div>
+    <div class="status"><h3>"playing"</h3></div>
     */
+    let GameUnit = $('#game-unit');
 
-    //some functions for updating the game-unit, mouseCount, catStory, and catData
+    let M = $('<div class="message">');
+    //if/else to handle currentRoom = 0
+    let message = `You are in the ${rooms[currentRoom]["name"]}`;
+    M.text = message;
+    let imageSource = `${rooms[currentRoom]["image"]}`;
+    let IMG = $('<img>');
+    IMG.attr({src:imageSource});  //add altlater
+    let OPT = $('<form class="options">');
+    ////need to plan out the options form with multiple buttons, scaling depending on the number of buttons
+    let status = `${rooms[currentRoom]["status"]}`;
+    let STAT = $('<div class="status">');
+    STAT.text = status;
+
+    //still need to addchild to dom and append to GameUnit.
+    //then start debugging
+
+
+    //some functions for updating the currentRoom, game-unit, mouseCount, catStory, and catData
     //functions for posting catStory and catData to db. i.e. buttons hit post routes on server
 
     // M V P
