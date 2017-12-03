28963d58769640c54a8d248ae1a5107df4349390
Craig Dempsey <craigdempsey@gmail.com>
2017-12-03 18:23:35 -0500

    show results page also fix of algorithm for sortbylastoccurance

--- public/JS/results.js
+++ public/JS/results.js
@@ -1,11 +1,4 @@
-let allCatData;
 let myCatData = $('#cat-data').val().split(',');
-
-let averagedStoryLength;
-let sortedByStoryLength;
-let sortedByNumberOfTwosAndFours;
-let sortedByLastOccurance;
-
 let dataContainer = {
   myCatCaution: myCatData.length,
   cautionData: [],
@@ -19,15 +12,16 @@ let dataContainer = {
 // what is a more environment-specific url?
 d3.json('http://localhost:3000/results/results', function(data) {
 
-  allCatData = data[0].all_cat_array;
+
+  let allCatData = data[0].all_cat_array;
 
   // console.log("all cat data", allCatData);
   // console.log("my cat data", myCatData);
 
-  averagedStoryLength = averageStoryLength(allCatData);
-  sortedByStoryLength = sortByLength(allCatData);
-  sortedByNumberOfTwosAndFours = sortByNumberOfTwosAndFours(allCatData);
-  sortedByLastOccurance = sortByLastOccurance(allCatData);
+  let averagedStoryLength = averageStoryLength(allCatData);
+  let sortedByStoryLength = sortByLength(allCatData);
+  let sortedByNumberOfTwosAndFours = sortByNumberOfTwosAndFours(allCatData);
+  let sortedByLastOccurance = sortByLastOccurance(allCatData);
 
   dataContainer.cautionData = sortedByStoryLength.map(arr => {
     return arr.length;
@@ -108,56 +102,107 @@ function sortByLastOccurance(arrays) {
       }
     });
     let numB = b.reduce( (acc,i) => {
-      if (i[i.length-1]==7) {
-        return 1;
-      } else {
+      if (i[i.length-2]==6) {
         return 0;
+      } else {
+        return 1;
       }
     });
-    return numA - numB;
+    return numB - numA;
   });
   return newArrayOfArrays;
 }
 
 
+
 function createCharts (data) {
   // myCatCaution, cautionData, myCatAggression, agressionData, myCatAdventerousness, adventerousnessData
-  let ChartContainer = d3.select('#chart-container');
-  ChartContainer.text("Cautiousness");
-  let Chart1 = ChartContainer.append('div');
-  let chart1Data = data.cautionData;
   let colors = [
-    "#E7826F",
-    "#EC837A",
-    "#EF8486",
-    "#F08693",
-    "#F0899F",
-    "#ED8DAC",
-    "#E993B8",
-    "#E298C4",
-    "#DA9FCE",
-    "#D0A6D8",
-    "#C5ADE0",
-    "#B7B4E6",
-    "#A9BBEB",
-    "#9AC1ED",
-    "#8BC8EE",
-    "#7CCEED",
-    "#6ED3EA",
-    "#63D8E4",
-    "#5CDDDE",
-    "#59E1D5"
+    "#F18662","#E88D52","#DA9646","#C89E3F","#B5A53F","#9FAC46","#89B153","#72B565","#59B879","#40B98E","#28BAA3","#20B9B6","#35B6C7","#55B2D3","#76ADDA","#96A6DB","#B49ED6","#CE95CC","#E38DBE","#F387AC"
   ];
 
-  Chart1
-    .attr('class','chart')
+  // let myCatCaution = $('.result1');
+  // myCatCaution.text(data.myCatCaution);
+  let block1 = $('.color-block1');
+  block1.css('background-color', colors[data.myCatCaution]);
+  let Chart1 = d3.select('.chart1').append('div');
+  let chart1Data = data.cautionData;
+  console.log(chart1Data.length, 'length');
+  let calculateItemWidth = function () {
+    let data = chart1Data;
+    let str = '';
+    let width = 100/data.length;
+    return str + width+'%';
+  };
+
   Chart1.selectAll('div')
     .data(chart1Data)
     .enter().append('div')
     .style('display', 'inline-block')
-    .style('width', '0.9%')
-    .style('height', '100%')
-    .style('background-color', function (d) { return colors[d]; });
+    .style('width', calculateItemWidth())
+    .style('height', '1em')
+    .style('background-color', function (d) {
+      let color = d;
+      if (color > colors.length-1) {
+        return colors[colors.length-1];
+      } else {
+      return colors[d];
+      }
+    });
+
+
+///////////////////////////////////////////
+
+  // let myCatAggression = $('.result2');
+  // myCatAggression.text(data.myCatAggression);
+  let block2 = $('.color-block2');
+  block2.css('background-color', colors[(data.myCatAggression*2)]);
+  let Chart2 = d3.select('.chart2').append('div');
+  let chart2Data = data.aggressionData;
+
+
+  Chart2.selectAll('div')
+    .data(chart2Data)
+    .enter().append('div')
+    .style('display', 'inline-block')
+    .style('width', calculateItemWidth())
+    .style('height', '1em')
+    .style('background-color', function (d) {
+      let color = d*2;
+      if (color > colors.length-1) {
+        return colors[colors.length-1];
+      } else {
+      return colors[d*2];
+      }
+  });
+
+///////////////////////////////////////////
+
+  let findColor = function (d) {
+    if (d == 7) {
+        return colors[colors.length-10];
+      } else {
+      return colors[0];
+      }
+  };
+  // let myCatAdventurousness = $('.result3');
+  // myCatAdventurousness.text(data.myCatAdventurousness);
+  let block3 = $('.color-block3');
+  block3.css('background-color', findColor(data.myCatAdventurousness));
+  let Chart3 = d3.select('.chart3').append('div');
+  let chart3Data = data.adventurousnessData;
+
+
+  Chart3.selectAll('div')
+    .data(chart3Data)
+    .enter().append('div')
+    .style('display', 'inline-block')
+    .style('width', calculateItemWidth())
+    .style('height', '1em')
+    .style('background-color', function(d) {
+      return findColor(d);
+    });
+
 }
 
 
