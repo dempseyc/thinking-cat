f46a4b1b917bf21249e22d86b9ac80c9aa5147d1
Craig Dempsey <craigdempsey@gmail.com>
2017-12-03 21:15:37 -0500

    change port to process env port

--- app.js
+++ app.js
@@ -209,6 +209,6 @@ app.get('/logout', function(req, res){
   res.redirect('/');
 });
 
-app.listen(3000, function () {
-  console.log('listening on port 3000!');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
